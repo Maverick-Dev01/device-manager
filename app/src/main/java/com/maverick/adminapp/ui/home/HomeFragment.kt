bce8b5ae49f2f58a5e6a7f778f8d1decc210c2f6
SalvadorAndrade02 <salvadormarcelianoandrade02@gmail.com>
2025-03-26 14:08:25 -0600

    [feature/add_HamburgerMenu] Modificar fragment_home para la inclusion del menu

--- app/src/main/java/com/maverick/adminapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/maverick/adminapp/ui/home/HomeFragment.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.maverick.adminapp.R
 import com.maverick.adminapp.adapters.DeviceAdapter
@@ -37,9 +46,50 @@ class HomeFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar?.hide()
 
         // Inicializar vistas
+        val drawerLayout = view.findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        val navView = view.findViewById<NavigationView>(R.id.navigation_view)
+        val header = navView.getHeaderView(0)
+        val tvUserName = header.findViewById<TextView>(R.id.tvUserName)
+        val tvUserEmail = header.findViewById<TextView>(R.id.tvUserEmail)
+
+        val btnClose = header.findViewById<ImageButton>(R.id.btnCloseDrawer)
+
+        btnClose.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+        toolbar.title = "Dispositivos Registrados"
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        tvUserName.text = user?.displayName ?: "Nombre no disponible"
+        tvUserEmail.text = user?.email ?: "Correo no disponible"
         recyclerDevices = view.findViewById(R.id.recyclerViewDevices)
         fabAddDevice = view.findViewById(R.id.fab_add_device)
 
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_logout -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    cerrarSesion()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        // Este método muestra el ícono hamburguesa a la izquierda del título
+        val toggle = ActionBarDrawerToggle(
+            activity,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
         setupRecyclerView()
         loadDevicesFromFirestore()
 
@@ -104,6 +154,16 @@ class HomeFragment : Fragment() {
             .addOnFailureListener { e ->
                 e.printStackTrace()
             }
+    }
+    private fun cerrarSesion() {
+        // --- OPCIÓN A: Si usas Firebase ---
+        FirebaseAuth.getInstance().signOut()
+
+        // Mostrar confirmación
+        Toast.makeText(requireContext(), "Sesión cerrada", Toast.LENGTH_SHORT).show()
+
+        // Redirigir al LoginFragment (si usas Navigation Component)
+        findNavController().navigate(R.id.loginFragment)
     }
 
 }
